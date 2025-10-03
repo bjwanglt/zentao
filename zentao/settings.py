f18aff569a9aeecb6afcf5d855d3bb01738a4129
wangliangtian <2597843280@qq.com>
2025-10-03 15:31:40 +0800

    网站配置

--- zentao/settings.py
+++ zentao/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-kjm*-kip!tgy1n4o&8s&dbu1^h#lrv05tdiv@l0rcqh+ehxcv=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'zentao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
